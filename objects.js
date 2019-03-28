0007edd93f82f0c2e8a6ffd1888bb155b77f74dc
Tanuka Das <das.tanuka@yahoo.com>
2019-03-28 04:52:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playlist = {
 //                       object, key(string), value
 function updatePlaylist(playlist, artistName, songTitle){
   //Taylor : "Back to December"
-  playlist.Williams = "happy";
+  playlist.Taylor = "happy";
   //playlist [Taylor] = "Back to December"
   //return Object.assign({}, playlist, {[artistName] : "songTitle"})
   //return {Object (Taylor, Slowdive, My Bloody Valentine)}
