978c8ce97cb75469fb7782ccf13afa23e79457d5
Tanuka Das <das.tanuka@yahoo.com>
2019-03-28 05:16:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,7 +8,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistName){
+object removeFromPlaylist(playlist, artistName){
   //delete playlist.Taylor;
   //delete playlist['Taylor']
   delete playlist.artistName
